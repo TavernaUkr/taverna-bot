1581b3cc831e0189fd726ce53f35951c0dde1e2a
TavernaUkr <taverna.ukr@gmail.com>
2025-09-11 03:55:20 +0300

    Update bot_updated.py

--- bot_updated.py
+++ bot_updated.py
@@ -26,7 +26,7 @@ from flask import Flask
 from aiogram import Bot, Dispatcher, Router, F
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from aiogram.filters import CommandStart
+from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.storage.memory import MemoryStorage
